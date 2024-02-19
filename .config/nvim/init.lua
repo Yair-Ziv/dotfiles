81ea15ec2952cca11ce942ac94dfd69abae01973
Yair Ziv <zivyair06@gmail.com>
2024-02-20 00:40:59 +0200

    Set default shiftwidth tabstop and expand tab to all filetypes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -60,6 +60,10 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+
 -- [[ Configure plugins ]]
 -- NOTE: Here is where you install your plugins.
 --  You can configure plugins using the `config` key.
