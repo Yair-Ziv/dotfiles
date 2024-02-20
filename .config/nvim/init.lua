a19d5d39c30fb8c4aa057cfcb7d713ae9e6f077b
Yair Ziv <zivyair06@gmail.com>
2024-02-20 16:55:02 +0200

    Added colorizer plugin to nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -81,6 +81,7 @@ require('lazy').setup({
 
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
+  'norcalli/nvim-colorizer.lua',
 
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
