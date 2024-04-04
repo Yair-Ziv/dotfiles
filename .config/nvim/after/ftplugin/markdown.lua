00556ce754ab13c1e53d5922d256aa5fc852ae25
Yair Ziv <zivyair06@gmail.com>
2024-04-04 11:36:05 +0300

    Fixed markdown ftplugin missing characters

--- .config/nvim/after/ftplugin/markdown.lua
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -1,6 +1,6 @@
-vim.opt.tabsto = 2
-vim.opt.shiftwidt = 2
-vim.opt.expandta = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
 vim.opt.colorcolumn = "100"
 vim.opt.autoindent = true
 vim.opt.linebreak = true
