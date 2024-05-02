8f9d11995569ac07ff9fd61968b8a667d118e116
Yair Ziv <zivyair06@gmail.com>
2024-05-02 14:09:38 +0300

    Fixed todo-comments plugin

--- .config/nvim/lua/yair/plugins/todo-comments.lua
+++ .config/nvim/lua/yair/plugins/todo-comments.lua
@@ -1,6 +1,11 @@
 -- Handles todo comments, not yet working
+-- HACK This is a warning
+-- TODO: This is an example for todo
+-- TEST this is an example for test
+-- FIXME This is an example for fixme
 return {
-  "folke/todo-comments.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  dependencies = { "nvim-lua/plenary.nvim" },
+  'folke/todo-comments.nvim',
+  event = 'VimEnter',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  opts = { signs = false }
 }
