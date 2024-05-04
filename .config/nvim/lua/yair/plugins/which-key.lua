ee99eb9992d9a98e8b5f5671e3720e0f7f903bc5
Yair Ziv <zivyair06@gmail.com>
2024-05-04 14:59:59 +0300

    Added Cloak shortcuts

--- .config/nvim/lua/yair/plugins/which-key.lua
+++ .config/nvim/lua/yair/plugins/which-key.lua
@@ -17,6 +17,7 @@ return {
       ['<leader>e'] = { name = '[E]xplorer', _ = 'which_key_ignore'},
       ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
       ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+      ['<leader>k'] = { name = 'Cloa[K]', _ = 'which_key_ignore' },
       ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
       ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
       ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
@@ -37,5 +38,14 @@ return {
     wk.register{
       ['<leader>gi'] = { ":Git<CR>", "Open [G]it [I]nteractive" },
     }
+
+    -- Cloak
+    wk.register {
+      ['<leader>kt'] = { ':CloakToggle<CR>', 'Toggle Cloak' },
+      ['<leader>ke'] = { ':CloakEnable<CR>', 'Cloak Enable' },
+      ['<leader>kd'] = { ':CloakDisable<CR>', 'Cloak Disable' },
+      ['<leader>kp'] = { ':CloakPreviewLine<CR>', 'Preview Cloak' },
+    }
+
   end
 }
