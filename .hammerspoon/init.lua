160c23703756a33a4e8bfff1dd2a4f871cb298e7
Yair Ziv <zivyair06@gmail.com>
2025-08-19 11:38:31 +0300

    Created new aliases and cleaned up hammerspoon shortcuts

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,42 +1,27 @@
 local baseShortcuts = {"cmd", "alt", "ctrl"}
 
-hs.hotkey.bind(baseShortcuts, "t", function()
-    hs.application.launchOrFocus("Ghostty")
-end)
+local function open(name)
+    return function()
+        hs.application.launchOrFocus(name)
+    end
+end
 
-hs.hotkey.bind(baseShortcuts, "r", function()
-    hs.application.launchOrFocus("Rider")
-end)
+hs.hotkey.bind(baseShortcuts, "t", open("Ghostty"))
 
-hs.hotkey.bind(baseShortcuts, "d", function()
-    hs.application.launchOrFocus("DataGrip")
-end)
+hs.hotkey.bind(baseShortcuts, "r", open("Rider"))
 
-hs.hotkey.bind(baseShortcuts, "a", function()
-    hs.application.launchOrFocus("Google Chrome")
-end)
+hs.hotkey.bind(baseShortcuts, "d", open("DataGrip"))
 
-hs.hotkey.bind(baseShortcuts, "o", function()
-    hs.application.launchOrFocus("OpenLens")
-end)
+hs.hotkey.bind(baseShortcuts, "a", open("Google Chrome"))
 
-hs.hotkey.bind(baseShortcuts, "w", function()
-    hs.application.launchOrFocus("WebStorm")
-end)
+hs.hotkey.bind(baseShortcuts, "o", open("OpenLens"))
 
-hs.hotkey.bind(baseShortcuts, "m", function()
-    hs.application.launchOrFocus("Microsoft Outlook")
-end)
+hs.hotkey.bind(baseShortcuts, "w", open("WebStorm"))
 
-hs.hotkey.bind(baseShortcuts, "s", function()
-    hs.application.launchOrFocus("Slack")
-end)
+hs.hotkey.bind(baseShortcuts, "m", open("Microsoft Outlook"))
 
-hs.hotkey.bind(baseShortcuts, "c", function()
-    hs.application.launchOrFocus("Cursor")
-end)
+hs.hotkey.bind(baseShortcuts, "s", open("Slack"))
 
-hs.hotkey.bind(baseShortcuts, "f", function()
-    hs.application.launchOrFocus("Firefox")
-end)
+hs.hotkey.bind(baseShortcuts, "c", open("Cursor"))
 
+hs.hotkey.bind(baseShortcuts, "f", open("Firefox"))
